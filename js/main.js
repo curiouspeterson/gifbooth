1925e6b94d75fef8e4b8fdb7a08c1ed811ab7c88
Adam Peterson <adam@cultivatedwit.com>
2014-05-16 14:47:25 -0700

    infinite scroll

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 $(document).ready(function() {
+	
+	var initialLoad = true;
+	var gifOffset = 0; 
+	var gifOffsetString = '&offset='+gifOffset+'';
 
 	jsoncallback = function(data) {
+		gifCount = data.pagination.count;
+		gifOffset = gifOffset + data.pagination.count;
+		gifOffsetString = '&offset='+gifOffset+'';
+		console.log('success callback '+gifOffset+'');
+		var totalCount = data.pagination.total_count;
 		var numNew = 0;
-		console.log(data);
+
 		var gifResults = data.pagination.count;
-
-		for (i = 0; i < gifResults; i++) {
+		
+		
+		if (gifOffset < totalCount) {
 			
-				$currentGif = data.data[i];
-				$originalImg = $currentGif.images.original;
-				$newString = '<div id="' + $currentGif.id +'-container" class="image-container"><div class="share-hover"></div><img data-original-width="' + $originalImg.width +'" data-original-height="' + $originalImg.height +'" data-padding-top="0" data-id="' + $currentGif.id +'" data-image_url="' + $originalImg.url +'" data-bitly_gif_url="' + $currentGif.bitly_gif_url +'" data-tumblr_share_url="' + $originalImg.url +'" data-gif_id="' + $currentGif.id +'" item_prop="content_url" id="gif" class="a-gif" src="' + $originalImg.url +'" alt="Animation Domination High-Def animated GIF" style="width: 400px; height: 400px" width="400" height="400"></div>';
+			for (i = 0; i < gifResults; i++) {
+			
+					$currentGif = data.data[i];
+					$originalImg = $currentGif.images.original;
+				
+					$shareLinks = '<a class="facebook-share" href="http://www.facebook.com/sharer.php?u=' + $currentGif.bitly_gif_url +'" title="Comedy Hack Day Gif Booth" target="_blank">share on facebook</a> <a class="twitter-share" href="http://twitter.com/share?url=' + $currentGif.bitly_gif_url +'?tc=1&amp;via=Comedy+Hack+Day" title="Comedy Hack Day Gif Booth" target="_blank">Share on twitter</a>';
+				
+				
+				
+					$newString = '<div id="' + $currentGif.id +'-container" class="image-container"><div class="share-hover">'+ $shareLinks +'</div><img data-original-width="' + $originalImg.width +'" data-original-height="' + $originalImg.height +'" data-padding-top="0" data-id="' + $currentGif.id +'" data-image_url="' + $originalImg.url +'" data-bitly_gif_url="' + $currentGif.bitly_gif_url +'" data-tumblr_share_url="' + $originalImg.url +'" data-gif_id="' + $currentGif.id +'" item_prop="content_url" id="gif" class="a-gif" src="' + $originalImg.url +'" alt="Animation Domination High-Def animated GIF" style="width: 400px; height: 400px" width="400" height="400"></div>';
 
-        $('#results').prepend($newString);
+	        $('#results').prepend($newString);
+					initialLoad = false;
 
+			}
+			
+			
 		}
 
 		while (numNew > 0) {
@@ -21,7 +42,7 @@ $(document).ready(function() {
 			//var postString = 'http://gifboothtesting.tumblr.com ' + newID + '';
 			//console.log(postString);
 			//$('#posts').load(postString);
-			console.log(newID);
+
 
 			$.post("http://gifboothtesting.tumblr.com", function(data) {
 				var newContent = $(data).find(newID);
@@ -33,11 +54,16 @@ $(document).ready(function() {
 	};
 
 	function apiCall() {
+		console.log('api call '+gifOffset+'');
+		
+		var apiURL = 'http://api.giphy.com/v1/gifs/search?q=foxadhd&api_key=dc6zaTOxFJmzC'+gifOffsetString+'';
+		console.log(apiURL);
 		$.ajax({
 			method: "GET",
-			url: 'http://api.giphy.com/v1/gifs/search?q=foxadhd&api_key=dc6zaTOxFJmzC',
+			url: apiURL,
 			datatype: 'json',
 			success: function(data) {
+				
 				jsoncallback(data);
 			},
 			error: function(jqXHR, textStatus, errorThrown) {
@@ -45,6 +71,18 @@ $(document).ready(function() {
 			}
 		});
 	}
+	
+	$('#pagination-trigger').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+	  if (isInView) {
+			console.log('in view');
+			if (initialLoad !== true) {
+				apiCall();
+			} 
+			
+	  } else {
+			console.log('not in view');
+	  }
+	});
 
 	apiCall();
 	
